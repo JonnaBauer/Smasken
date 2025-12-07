3f0decbcb6a06a89abb3723d47c6b02f6e267048
HectorBergman <hectorbergman02@gmail.com>
2025-12-07 12:48:58 +0100

    87

--- fucknotebook copy 2.py
+++ fucknotebook copy 2.py
@@ -18,7 +18,9 @@ df["increase_stock"] = df["increase_stock"].map({
     "low_bike_demand": 0
 })
 
-#new metrics:
+#######################
+# new metrics:
+
 df["weather_severity"] = (
       df["precip"] * 2
     + df["snow"] * 3
@@ -26,19 +28,42 @@ df["weather_severity"] = (
     + (df["windspeed"] > 15).astype(int) * 2
     + (df["visibility"] < 5).astype(int) * 2
 )
-df["rush_hour"] = ((df["hour_of_day"].between(7, 9)) | 
+df["rush_hour_severity"] = ((df["hour_of_day"].between(7, 9)) | 
                    (df["hour_of_day"].between(16, 18))).astype(int)
+df["time_off"] = ((1 - df["weekday"]) | 
+                   (df["holiday"]))
 
-#
+conditions = [ #based on the "volume of high bike demand by hour" graph
+    df["hour_of_day"].between(8, 14),
+    df["hour_of_day"].between(15, 19),
+]
+values = [1, 2]
+df["rush_hour_severity"] = np.select(conditions, values, default=0)
 
-X = df[["weather_severity", "hour_of_day", "weekday"]]
+
+def season(month):
+    if month in [12, 1, 2]:  #winter
+        return 0
+    elif month in [3, 4, 5]: #spring
+        return 1
+    elif month in [6, 7, 8]: #summer
+        return 2
+    else:                    #autumn
+        return 3
+
+df["season"] = df["month"].apply(season)
+
+######################
+
+
+X = df[["weather_severity", "rush_hour_severity", "time_off", "season"]]
 
 Y = df["increase_stock"]
 
-random_states = range(0,100)  # change as needed
+random_states = range(0,10)  
 
-# DataFrame to collect results
-all_results = pd.DataFrame()
+
+all_results = pd.DataFrame() 
 for rs in random_states:
     print(f"Running GridSearch for random_state = {rs}")
 
@@ -46,7 +71,7 @@ for rs in random_states:
         X, Y, test_size=0.2, random_state=rs
     )
     
-    k_amt = 101 
+    k_amt = 71
     k_range = range(1,k_amt+1, 2)
 
     knn = KNeighborsClassifier()
@@ -57,7 +82,7 @@ for rs in random_states:
     )
     grid_search.fit(X_train, y_train)
 
-    # Extract results
+
     results = pd.DataFrame(grid_search.cv_results_)
     results["random_state"] = rs
 
@@ -76,15 +101,13 @@ avg_scores = (
     .reset_index()
 )
 
-# Sort by best k value (highest avg score)
+
 final_ranking = avg_scores.sort_values(
     by="mean_accuracy",
     ascending=False
 ).reset_index(drop=True)
 
 
-# ------------------------------------------------------------------------------------
-# Print results
-# ------------------------------------------------------------------------------------
+
 print("\n===== FINAL AVERAGED RANKING ACROSS RANDOM STATES =====\n")
 print(final_ranking)
